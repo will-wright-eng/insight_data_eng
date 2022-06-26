d33fcc728d65a5e5688dbdafc250eb9599a58966
william-cass-wright <william.cass.wright@gmail.com>
2022-06-26 01:41:19 -0700

    black

--- data_sampling/preprocessing.py
+++ data_sampling/preprocessing.py
@@ -1,10 +1,10 @@
-'''
+"""
 preprocessing wet path files
 saves csv lists and config file locally and in S3
 csv list is input to spark app
 
 Author: William Wright
-'''
+"""
 
 import os
 import gzip
@@ -18,51 +18,59 @@ import boto3
 import config_preprocess
 
 
-LOG_LEVEL = 'INFO'
-LOGGING_FORMAT = '%(asctime)s %(levelname)s %(name)s: %(message)s'
+LOG_LEVEL = "INFO"
+LOGGING_FORMAT = "%(asctime)s %(levelname)s %(name)s: %(message)s"
 logging.basicConfig(level=LOG_LEVEL, format=LOGGING_FORMAT)
 
 
-def save_config(today, new_dir, filename_inputcsv, bucket,
-                filename_outputparquet, n):
-    '''save_config docstring'''
+def save_config(today, new_dir, filename_inputcsv, bucket, filename_outputparquet, n):
+    """save_config docstring"""
     lines = [
-        "'''\n", "config file for anything that shouldn't be on github\n",
-        "'''\n", "input_file = '" + today + "_" + filename_inputcsv + "'\n",
-        "output_file = 's3://" + bucket + "/results/" + new_dir + "/" +
-        filename_outputparquet + "'\n" + "file_n = " + str(n)
+        "'''\n",
+        "config file for anything that shouldn't be on github\n",
+        "'''\n",
+        "input_file = '" + today + "_" + filename_inputcsv + "'\n",
+        "output_file = 's3://"
+        + bucket
+        + "/results/"
+        + new_dir
+        + "/"
+        + filename_outputparquet
+        + "'\n"
+        + "file_n = "
+        + str(n),
     ]
-    with open('config.py', 'w') as file:
+    with open("config.py", "w") as file:
         for line in lines:
             file.write(line)
 
 
 def save_projectfiles_tos3(new_dir, cwd, bucket):
-    '''
+    """
     filename: local file or filepath
     bucket: name for boto3 regex search
-    _key: new directory path with filename appended'''
+    _key: new directory path with filename appended"""
     try:
         os.chdir(new_dir)
         files = os.listdir()
         for file in files:
-            _key = 'results/' + new_dir + '/' + file
-            logging.info('upload to S3: %s', _key)
-            s3 = boto3.resource('s3')
+            _key = "results/" + new_dir + "/" + file
+            logging.info("upload to S3: %s", _key)
+            s3 = boto3.resource("s3")
             s3.meta.client.upload_file(Filename=file, Bucket=bucket, Key=_key)
     finally:
         os.chdir(cwd)
 
 
 def extract_wet_paths(wet_filename):
-    '''extract_wet_paths docstring'''
-    with gzip.GzipFile(wet_filename, mode='r') as file:
-        paths = [x.decode('utf8').strip() for x in file.readlines()]
+    """extract_wet_paths docstring"""
+    with gzip.GzipFile(wet_filename, mode="r") as file:
+        paths = [x.decode("utf8").strip() for x in file.readlines()]
     return paths
 
 
 def gen_rand_seq(paths, n):
-    '''gen_rand_seq docstring'''
+    """gen_rand_seq docstring"""
     rand_sequence = [random.randint(0, len(paths) - 1) for i in range(n)]
     if len(set(rand_sequence)) == n:
         return rand_sequence
@@ -71,13 +79,13 @@ def gen_rand_seq(paths, n):
 
 
 def process_files(files, n):
-    '''process_files docstring'''
+    """process_files docstring"""
     temp = []
     dir_stats = []
     for wet_filename in files:
         paths = extract_wet_paths(wet_filename)
 
-        _set = paths[0].split('/segments')[0]
+        _set = paths[0].split("/segments")[0]
         _len = len(paths)
 
         rand_sequence = gen_rand_seq(paths, n)
@@ -88,58 +96,57 @@ def process_files(files, n):
     df = pd.DataFrame(pathlist)
 
     df_stats = pd.DataFrame(dir_stats)
-    df_stats.columns = ['wet_filename', 'set', 'length']
+    df_stats.columns = ["wet_filename", "set", "length"]
     return df, df_stats
 
 
 def check_newdir(new_dir):
-    '''check_newdir docstring'''
+    """check_newdir docstring"""
     if new_dir in os.listdir():
-        return check_newdir(new_dir + '_dup')
+        return check_newdir(new_dir + "_dup")
     else:
         return new_dir
 
 
 def main():
-    '''main docstring'''
-    today = str(dt.datetime.today()).split(' ')[0]
-    filename = config_preprocess.wet_path_series+'.csv'
+    """main docstring"""
+    today = str(dt.datetime.today()).split(" ")[0]
+    filename = config_preprocess.wet_path_series + ".csv"
     cwd = os.getcwd()
     n = 5
 
     # create list of wet files
     try:
-        os.chdir('wet_path_files')
-        files = [i for i in os.listdir() if 'wet.path' in i]
+        os.chdir("wet_path_files")
+        files = [i for i in os.listdir() if "wet.path" in i]
         df, df_stats = process_files(files, n)
     finally:
         os.chdir(cwd)
 
     # save csvs and config to project sub directory
     bucket = config_preprocess.bucket
-    new_dir = today + '_cc_process_subdir'
+    new_dir = today + "_cc_process_subdir"
     new_dir = check_newdir(new_dir)
 
     try:
         os.mkdir(new_dir)
         os.chdir(new_dir)
-        logging.info('dataframe length: %s', str(len(df)))
+        logging.info("dataframe length: %s", str(len(df)))
 
         df.to_csv(filename, index=False, header=False)
-        df_stats.to_csv(filename.replace('.csv', '_stats.csv'), index=False)
+        df_stats.to_csv(filename.replace(".csv", "_stats.csv"), index=False)
 
         filename_inputcsv = filename
-        filename_outputparquet = filename.replace('.csv', '_results.parquet')
-        save_config(today, new_dir, filename_inputcsv, bucket,
-                    filename_outputparquet, n)
+        filename_outputparquet = filename.replace(".csv", "_results.parquet")
+        save_config(today, new_dir, filename_inputcsv, bucket, filename_outputparquet, n)
     finally:
-        logging.info('files in {}: {}'.format(new_dir, str(os.listdir())))
+        logging.info("files in {}: {}".format(new_dir, str(os.listdir())))
         os.chdir(cwd)
 
     # save all project files to S3
     save_projectfiles_tos3(new_dir, cwd, bucket)
-    logging.info('files saved to sub directory and uploaded to S3')
+    logging.info("files saved to sub directory and uploaded to S3")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/spark_app.py
+++ src/spark_app.py
@@ -1,8 +1,8 @@
-'''
+"""
 spark job to be run via spark_job.sh bash script
 
 Author: William Wright
-'''
+"""
 
 from tempfile import NamedTemporaryFile
 from csv import reader
@@ -19,20 +19,20 @@ from pyspark.sql import SQLContext
 
 import config
 
-LOG_LEVEL = 'INFO'
-NAME = 'spark-cc-analysis'
-LOGGING_FORMAT = '%(asctime)s %(levelname)s %(name)s: %(message)s'
+LOG_LEVEL = "INFO"
+NAME = "spark-cc-analysis"
+LOGGING_FORMAT = "%(asctime)s %(levelname)s %(name)s: %(message)s"
 logging.basicConfig(level=LOG_LEVEL, format=LOGGING_FORMAT)
 
 
 def get_logger():
-    '''docstring for get_logger'''
+    """docstring for get_logger"""
     return logging.getLogger(NAME)
 
 
 def pathlist_from_csv(filename):
-    '''docstring for pathlist_from_csv'''
-    with open(filename, 'r') as read_obj:
+    """docstring for pathlist_from_csv"""
+    with open(filename, "r") as read_obj:
         csv_reader = reader(read_obj)
         list_of_rows = list(csv_reader)
     pathlist = [i[0] for i in list_of_rows]
@@ -40,57 +40,55 @@ def pathlist_from_csv(filename):
 
 
 def s3_key_filename_dict(paths):
-    '''docstring for s3_key_filename_dict'''
-    return {path: path.split('wet/')[-1] for path in paths}
+    """docstring for s3_key_filename_dict"""
+    return {path: path.split("wet/")[-1] for path in paths}
 
 
 def download_wet_from_s3(_key, _temp, s3client):
-    '''docstring for download_wet_from_s3'''
-    get_logger().info('Downloading ' + _key)
-    bucket = 'commoncrawl'
+    """docstring for download_wet_from_s3"""
+    get_logger().info("Downloading " + _key)
+    bucket = "commoncrawl"
     try:
         s3client.download_fileobj(bucket, _key, _temp)
     except botocore.client.ClientError as e:
         get_logger().error(e)
     _temp.seek(0)
-    get_logger().info('download complete')
+    get_logger().info("download complete")
 
 
 def process_files(iterator):
-    '''docstring for process_files
-    S3 client not thread-safe, initialize outside parallelized loop'''
-    no_sign_request = botocore.client.Config(
-        signature_version=botocore.UNSIGNED)
-    s3client = boto3.client('s3', config=no_sign_request)
+    """docstring for process_files
+    S3 client not thread-safe, initialize outside parallelized loop"""
+    no_sign_request = botocore.client.Config(signature_version=botocore.UNSIGNED)
+    s3client = boto3.client("s3", config=no_sign_request)
     for _key in iterator:
-        _temp = NamedTemporaryFile(mode='w+b', dir='tmp/')
+        _temp = NamedTemporaryFile(mode="w+b", dir="tmp/")
         try:
             download_wet_from_s3(_key, _temp, s3client)
-            brands = ['facebook', 'apple', 'amazon', 'netflix', 'google']
-            with open(_temp.name, 'rb') as stream:
+            brands = ["facebook", "apple", "amazon", "netflix", "google"]
+            with open(_temp.name, "rb") as stream:
                 for record in ArchiveIterator(stream):
-                    if record.rec_type == 'conversion':
+                    if record.rec_type == "conversion":
                         text = record.content_stream().read()
-                        text = text.decode('utf-8').lower().translate(
-                            str.maketrans('', '', string.punctuation))
-                        brand_count = [
-                            sum([1 if brand in text else 0])
-                            for brand in brands
-                        ]
-                        lines = text.split('\n')
-                        words = [i.split(' ') for i in lines]
+                        text = text.decode("utf-8").lower().translate(str.maketrans("", "", string.punctuation))
+                        brand_count = [sum([1 if brand in text else 0]) for brand in brands]
+                        lines = text.split("\n")
+                        words = [i.split(" ") for i in lines]
                         words = [item for sublist in words for item in sublist]
                         c = Counter(words)
                         brand_nums = [c[brand] for brand in brands]
                         # results [[wet file data], [brand simple count], [brand total count]]
-                        results = [[
-                            record.rec_headers.get_header('WARC-Target-URI'),
-                            record.rec_headers.get_header('WARC-Date'),
-                            record.rec_headers.get_header('Content-Length')
-                        ], brand_count, brand_nums]
+                        results = [
+                            [
+                                record.rec_headers.get_header("WARC-Target-URI"),
+                                record.rec_headers.get_header("WARC-Date"),
+                                record.rec_headers.get_header("Content-Length"),
+                            ],
+                            brand_count,
+                            brand_nums,
+                        ]
                         for i in range(len(brands)):
-                            yield tuple(results[0] + [brands[i]] +
-                                        [results[1][i]] + [results[2][i]])
+                            yield tuple(results[0] + [brands[i]] + [results[1][i]] + [results[2][i]])
         except ArchiveLoadFailed as e:
             get_logger().error(e)
         finally:
@@ -98,12 +96,11 @@ def process_files(iterator):
 
 
 def run():
-    '''docstring for run
+    """docstring for run
     set 'n' to length of key list
-    n = 69 * (number of files sampled)'''
+    n = 69 * (number of files sampled)"""
     n = 345
-    conf = SparkConf() \
-        .set("spark.default.parallelism", n)
+    conf = SparkConf().set("spark.default.parallelism", n)
 
     sc = SparkContext(appName=NAME, conf=conf)
 
@@ -114,15 +111,12 @@ def run():
 
     rdd = sc.parallelize(pathlist)
     results = rdd.mapPartitions(process_files)
-    cols = [
-        'target_uri', 'timestamp', 'content_length', 'entity', 'entity_count',
-        'entity_total'
-    ]
+    cols = ["target_uri", "timestamp", "content_length", "entity", "entity_count", "entity_total"]
     df = sqlc.createDataFrame(results, cols)
     df.show()
     output = config.output_file
-    df.write.mode('overwrite').parquet(output)
+    df.write.mode("overwrite").parquet(output)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
